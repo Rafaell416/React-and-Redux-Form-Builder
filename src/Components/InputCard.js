bff9e1388d0b7a6d3a0e91104f0cf536a0289603
Rafael Villarreal <rvillarreal416@gmail.com>
2017-09-21 03:30:46 -0500

    card styled

--- src/Components/InputCard.js
+++ src/Components/InputCard.js
@@ -3,7 +3,9 @@ import {
   Col,
   Card,
   Button,
-  Icon
+  Icon,
+  Row,
+  Input
 } from 'react-materialize'
 
 
@@ -12,16 +14,33 @@ export default class InputCard extends Component {
     return (
       <Col m={9} s={12}>
       		<Card key={1}
-            className='teal lighten-4'
-            textClassName='white-text'
-            title='Card title'
+            className='grey lighten-3'
             actions={
               [
-                <Button waves='light'>Add Sub-Input<Icon right>add</Icon></Button>,
-                <Button waves='light'>Delete<Icon right>delete</Icon></Button>
+                <div className='row'>
+                  <div className='col s12 m5 offset-m3'>
+                    <Button waves='light'>Add Sub-Input<Icon right>add</Icon></Button>
+                  </div>
+                  <div className='col s12 m4 right'>
+                    <Button waves='light'>Delete<Icon right>delete</Icon></Button>
+                  </div>
+                </div>
               ]
           }>
-      		  I am a very simple card.
+      		  <Row>
+              <Input placeholder="Type a Question Here" s={12} m={10} label="Question" />
+            </Row>
+            <Row>
+            	<Input s={12} m={10}
+                type='select'
+                label="Type"
+                defaultValue='yes/no'
+              >
+              		<option value='yes/no'>Yes/No</option>
+              		<option value='number'>Number</option>
+              		<option value='text'>Text</option>
+            	</Input>
+            </Row>
       		</Card>
       </Col>
     )
